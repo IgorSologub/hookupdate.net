52a0a13ac56e84791c56aa2c8ec63cfe90fa7f7a
IgorSologub <sologub355@gmail.com>
2022-07-27 08:50:22 +0300

    gg11

--- src/main/java/pages/base/BasePage.java
+++ src/main/java/pages/base/BasePage.java
@@ -166,6 +166,9 @@ public class BasePage {
                 }
             }
         }
+        if (!titleStatus) {
+            outputWrongTitle();
+        }
         return titleStatus;
     }
 
